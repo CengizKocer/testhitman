5d25bfa922ddca6c149b4b54fedabf4c550b9d8d
CengizKocer <c-ngiz@live.nl>
2023-03-31 16:56:56 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,3 @@
+Config = {}
+
+Config.PedCoords = vector3(-32.62, -1088.47, 26.42)
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Hitman Script'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+shared_script {
+    'config.lua'
+}
